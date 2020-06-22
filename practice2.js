46bc012447bb35d6dd98929d891ba2c93d589e9e
Hansen-Nick <hansen.nick95@gmail.com>
2020-06-21 23:23:42 -0500

    Finished 3 problems

---
+++ practice2.js
@@ -0,0 +1,28 @@
+// Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical, and false otherwise. The array will contain 4 elements.
+
+// Examples
+// testJackpot(["@", "@", "@", "@"]) ➞ true
+
+// testJackpot(["abc", "abc", "abc", "abc"]) ➞ true
+
+// testJackpot(["SS", "SS", "SS", "SS"]) ➞ true
+
+// testJackpot(["&&", "&", "&&&", "&&&&"]) ➞ false
+
+// testJackpot(["SS", "SS", "SS", "Ss"]) ➞ false
+// Notes
+// The elements must be exactly identical for there to be a jackpot.
+
+function testJackpot(arr) {
+  let newArr = arr.filter((item) => {
+    return arr[0] === item;
+  });
+
+  if (newArr.length === arr.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(testJackpot(["@", "@", "@", "1"]));
---
+++ practice3.js
@@ -0,0 +1,17 @@
+// Create a function that takes a string and returns the word count. The string will be a sentence.
+
+// Examples
+// countWords("Just an example here move along") ➞ 6
+
+// countWords("This is a test") ➞ 4
+
+// countWords("What an easy task, right") ➞ 5
+// Notes
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+function countWords(string) {
+  return string.split(" ").length;
+}
+
+console.log(countWords("Just an example here move along"));
